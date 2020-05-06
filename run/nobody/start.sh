30d950bdacf8603484c834e129f9482731a2f3b1
binhex <megalith01@gmail.com>
2020-05-06 14:46:09 +0100

    copy and softlink error page and default index page

--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -15,5 +15,20 @@ fi
 # soft link back to container
 ln -s /config/nginx/ /etc/nginx
 
+# if nginx folder exists in container then rename
+if [[ -d "/usr/share/nginx/html" && ! -L "/usr/share/nginx/html" ]]; then
+	mv /usr/share/nginx/html-backup 2>/dev/null || true
+fi
+
+# if nginx folder on /config doesnt exists then
+# copy nginx config defaults from container
+if [ ! -d /config/nginx/html ]; then
+	mkdir -p /config/nginx/html
+	cp /usr/share/nginx/html-backup/* /config/nginx/html/
+fi
+
+# soft link back to container
+ln -s /config/nginx/html/ /usr/share/nginx/html
+
 # run nginx daemonised
 nginx ; cat
